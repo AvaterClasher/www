eb4aa9ccad7e509ec5fe491a912725fb1a377727
AvaterClasher <avater.clasher@gmail.com>
2024-06-24 22:46:39 +0530

    feat: got an intial setup with styles and stuff going

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-async function Home() {
-  return <main className="text-3xl">Hello web</main>;
+export default async function Home() {
+  return (
+    <main>
+      <section className="flex items-center justify-center gap-20 p-5">
+        <div className="space-y-8 relative z-10">
+          <header>
+            <p className="text-muted-foreground text-lg font-extralight">Hi all. I am</p>
+          </header>
+        </div>
+      </section>
+    </main>
+  )
 }
-
-export default Home;
