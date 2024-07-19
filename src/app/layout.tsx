c906fbe3e446255b842c663879571e523c72ec59
AvaterClasher <avater.clasher@gmail.com>
2024-07-19 23:51:16 +0530

    chore: changed the default theme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body>
         <ThemeWrapper
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
